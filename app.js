bf8acc62358a050551fbfca02064f03c6a3b7dde
Harsh Verma <73462650+HARSHVRM@users.noreply.github.com>
2021-11-03 15:51:18 +0530

    adding script for heroku

--- app.js
+++ app.js
@@ -102,7 +102,8 @@ app.use((err,req,res,next)=>{
   res.status(statusCode).render('error',{err});                     
 })
 
-app.listen(5000,()=>{
-    console.log('server started');
+const port=process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log(`server started ${port}`);
 })
 
